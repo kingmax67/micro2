01f078b603bff8bd0bfacd628aae36114a2f9144
kingmax67 <kingsleyche67@yahoo.com>
2024-04-07 17:24:16 -0400

    text

--- wishlist-microservice-python/index.py
+++ wishlist-microservice-python/index.py
@@ -34,5 +34,5 @@ def product(product):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=1003)
     print('Wishlist Microservice Started...')
